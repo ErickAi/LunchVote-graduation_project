f96cea4f381bff02b60e1e6b2e252823e1c9f3e5
Erick Aytkulov <e.aytkulov@gmail.com>
2018-05-01 20:34:20 +0300

    fix layers for user

--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.service;
 
-import com.example.dao.GenericRepository;
+import com.example.dao.UserRepository;
 import com.example.domain.User;
 import com.example.util.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,26 +15,27 @@ import java.util.List;
 import static com.example.util.ValidationUtil.checkNotFound;
 import static com.example.util.ValidationUtil.checkNotFoundWithId;
 
-@Service
+@Service("userService")
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
-    private final GenericRepository<User, Integer> repository;
+    private final UserRepository repository;
 
     @Autowired
-    public UserServiceImpl(GenericRepository<User, Integer> repository) {
+    public UserServiceImpl(UserRepository repository) {
         this.repository = repository;
     }
 
     @CacheEvict(value = "users", allEntries = true)
+    @Transactional
     @Override
     public User create(User user) {
         Assert.notNull(user, "user must not be null");
         return repository.save(user);
     }
 
-    @Transactional
     @CacheEvict(value = "users", allEntries = true)
+    @Transactional
     @Override
     public void delete(int id) throws NotFoundException {
         checkNotFoundWithId(repository.existsById(id),id);
@@ -54,6 +55,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @CacheEvict(value = "users", allEntries = true)
+    @Transactional
     @Override
     public void update(User user) {
         Assert.notNull(user, "user must not be null");
@@ -64,5 +66,19 @@ public class UserServiceImpl implements UserService {
     public User getByEmail(String email) throws NotFoundException {
         Assert.notNull(email, "email must not be null");
         return checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Override
+    @Transactional
+    public void enable(int id, boolean enabled) {
+        User user = get(id);
+        user.setEnabled(enabled);
+        repository.save(user);
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Override
+    public void evictCache() {
     }
 }
